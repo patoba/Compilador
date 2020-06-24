6f83a12e39db835e1fbc09861a10ddc9e064f333
patoba <patobarrero@gmail.com>
2020-06-24 14:19:27 -0500

    main

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+extern int yyparse();
+extern FILE *yyin;
+
+int main(int argc, char const *argv[])
+{
+    FILE *f;
+    if(argc < 2){
+        printf("faltan argumentos\n");
+        return -1;        
+    }
+    f = fopen(argv[1], "r");
+    if(!f){
+        printf("El archivo no se puede abrir");
+        return -1;
+    }
+    yyin = f;
+    yyparse();
+    fclose(yyin);
+    return 0;
+}
