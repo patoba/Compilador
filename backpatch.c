c43a723df2affbdd2443632b40e5b0acba379792
Emanuel Flores <mufymes@gmail.com>
2020-06-24 06:18:10 -0500

    Added backpatch implementation

---
+++ backpatch.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "backpatch.h"
+
+INDEX *init_index() {
+    static int indices = 0;
+    INDEX *index = (INDEX*)malloc(sizeof(INDEX));
+
+    index->indice = indices++; // Tenemos un problema
+    index->next = NULL;
+
+    return index;
+}
+
+void finish_index(INDEX *index) {
+    free(index);
+}
+
+LINDEX *init_list_index(INDEX *index) {
+    LINDEX *list_index = (LINDEX*)malloc(sizeof(LINDEX));
+
+    if (index != NULL) {
+        list_index->head = NULL;
+        list_index->tail = NULL;
+        append_index(list_index, index);
+    }
+    else {
+        exit(EXIT_FAILURE);
+    }
+
+    return list_index;
+}
+
+void finish_list_index(LINDEX *lista) {
+    free(lista);
+}
+
+void append_index(LINDEX *list, INDEX *index) {
+    if (list->tail == NULL && list->head == NULL) {
+        list->head = index;
+        list->tail = index;
+    }
+    else {
+        list->tail->next = index;
+        list->tail = index;
+    }
+
+    list->tail->next = NULL;
+}
+
+LINDEX *combinar(LINDEX *list1, LINDEX *list2) {
+    LINDEX *new_list = (LINDEX*)malloc(sizeof(LINDEX));
+
+    list1->tail->next = list2->head;
+
+    new_list->head = list1->head;
+    new_list->tail = list2->tail;
+
+    free_list_index(list1);
+    free_list_index(list2);
+
+    return new_list;
+}
+
+void backpatch(CODE *code, LINDEX *list, char *label) {
+    CUAD  *temp_quad = code->head;
+    INDEX *temp_index;
+    
+    while(temp_quad->next != NULL) {
+        temp_index = list->head;
+        while(temp_index != NULL) {
+            if (strcmp(temp_index->indice, temp_quad->res) == 0) {
+                strcpy(temp_quad->res, label);
+            }
+            temp_index = temp_index->next;
+        }
+        temp_quad = temp_quad->next;
+    }
+}
--- backpatch.h
+++ backpatch.h
@@ -4,7 +4,7 @@
 #include "cuadruplas.h"
 
 typedef struct index {
-    char  *indice;
+    char *indice;
     INDEX *next;
 } INDEX;
 
@@ -14,15 +14,15 @@ typedef struct list_index {
 } LINDEX;
 
 INDEX *init_index(); // Reserva memoria para un nodo índice
-void finish_index(INDEX *i); // Libera memoria de un nodo índice
+void finish_index(INDEX *index); // Libera memoria de un nodo índice
 
-LINDEX *init_list_index(INDEX *i); // Reserva memoria para la lista de índices
-void finish_list_index(CODE *c); // Libera la memoria de la lista y de todos los nodos dentro
+LINDEX *init_list_index(INDEX *index); // Reserva memoria para la lista de índices
+void finish_list_index(LINDEX *lista); // Libera la memoria de la lista y de todos los nodos dentro
 
-void append_index(LINDEX *l, INDEX *i); // Agrega un nodo índice al final de la lista
+void append_index(LINDEX *list, INDEX *index); // Agrega un nodo índice al final de la lista
 
-LINDEX *combinar(LINDEX l1, LINDEX l2); // Retorna una lista ligada de la concateción de l1 y l2
+LINDEX *combinar(LINDEX *list1, LINDEX *list2); // Retorna una lista ligada de la concateción de l1 y l2
 
-void backpatch(CODE *c, LINDEX *l, char *label); // Remplaza label en cada aparición de in índice en la cuádruplas del código C
+void backpatch(CODE *code, LINDEX *list, char *label); // Remplaza label en cada aparición de un índice en la cuádruplas del código C
 
 #endif
